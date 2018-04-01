3ddc34b250144456a1a92f1ce2dda114ed03da36
天 徐 <xutian0521@outlook.com>
2018-04-01 15:39:03 +0800

    first commit

---
+++ LittleSexy.Api/Controllers/ValuesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LittleSexy.Api.Controllers
+{
+    //[Route("[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        //[HttpGet]
+        public IEnumerable<string> Gets()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet]
+        public string Get(int id)
+        {
+            var ip= HttpContext.Request.Host;
+            return "Hello ! Wellcome to little sexy";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
